d2d8dacf4b68af81b43a19fef95f7315eefd5bf7
DySha <DySha@DESKTOP-2HEN19R>
2024-09-19 14:25:25 +0500

    dsfdsfs

--- Test1/Source.cpp
+++ Test1/Source.cpp
@@ -13,36 +13,36 @@ int main()
 
     std::cout << "Введите перове число --> ";
     std::cin >> Number1;
-    std::cout << "\nВведите второе число --> ";
-    std::cin >> Number2;
     std::cout << "\nВведите операцию ( + , - , * , / , % ) --> ";
     std::cin >> Operation;
+    std::cout << "\nВведите второе число --> ";
+    std::cin >> Number2;
 
     if (Operation == '+')
     {
         Sum = Number1 + Number2;
     }
-    if (Operation == '-')
-    {
-        Sum = Number1 - Number2;
-    }
-    if (Operation == '*')
-    {
-        Sum = Number1 * Number2;
-    }
-    if (Operation == '/')
-    {
-        if (Number2 == 0)
+    else if (Operation == '-')
         {
-            std::cout << "\nНизя на 0 делить";
-            return 0;
+            Sum = Number1 - Number2;
         }
-        Sum = Number1 / Number2;
-    }
-    if (Operation == '%')
-    {
-        Sum = (Number1 * 0.01) * Number2;
-    }
+    else if (Operation == '*')
+        {
+            Sum = Number1 * Number2;
+        }
+    else if (Operation == '/')
+        {
+            if (Number2 == 0)
+            {
+                std::cout << "\nНизя на 0 делить";
+                return 0;
+            }
+            Sum = Number1 / Number2;
+        }
+    else if (Operation == '%')
+        {
+            Sum = (Number1 * 0.01) * Number2;
+        }
 
     std::cout << "\n" << Number1 << " " << Operation << " " << Number2 << " = " << Sum;
 
